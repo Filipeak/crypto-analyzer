09039353e869d68d77d87f754e031ebdd61a1dd2
Filip Gawlik <filippo.gawlik@gmail.com>
2025-10-09 19:01:12 +0200

    Improve downloaders code, create utils class

--- src/main/java/com/newsscraper/sources/BinanceDownloader.java
+++ src/main/java/com/newsscraper/sources/BinanceDownloader.java
@@ -5,16 +5,8 @@ import com.newsscraper.data.WebDataFrame;
 import com.newsscraper.data.WebSource;
 import com.newsscraper.data.WebSourceStatus;
 import com.newsscraper.logging.Logger;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class BinanceDownloader implements WebSource {
 
@@ -25,44 +17,14 @@ public class BinanceDownloader implements WebSource {
 
     @Override
     public WebSourceStatus downloadFromWeb() {
-        URL url;
+        String jsonStr = CryptoHTTPDownloadUtils.fetch("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=24");
 
-        try {
-            url = URI.create("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=24").toURL();
-        } catch (MalformedURLException e) {
-            Logger.error(e.getMessage());
-
-            return WebSourceStatus.FAILURE_NO_DATA;
-        }
-
-        HttpURLConnection conn;
-
-        try {
-            conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.getResponseCode();
-        } catch (IOException e) {
-            Logger.error(e.getMessage());
-
+        if (jsonStr == null) {
             return WebSourceStatus.FAILURE_NO_INTERNET;
         }
 
-        StringBuilder jsonStr = new StringBuilder();
-
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                jsonStr.append(line);
-            }
-        } catch (Exception e) {
-            Logger.error(e.getMessage());
-
-            return WebSourceStatus.FAILURE_NO_DATA;
-        }
-
         try {
-            JSONArray jsonArray = new JSONArray(jsonStr.toString());
+            JSONArray jsonArray = new JSONArray(jsonStr);
 
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONArray element = jsonArray.getJSONArray(i);
