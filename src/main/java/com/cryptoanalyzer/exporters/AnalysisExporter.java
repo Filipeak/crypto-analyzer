bad9c2c61589b5315b4b90d6f6a4142327632108
Filip Gawlik <filippo.gawlik@gmail.com>
2025-10-21 09:55:04 +0200

    Add simple volume and volatility analysis

--- src/main/java/com/cryptoanalyzer/exporters/AnalysisExporter.java
+++ src/main/java/com/cryptoanalyzer/exporters/AnalysisExporter.java
@@ -3,7 +3,15 @@ package com.cryptoanalyzer.exporters;
 import com.cryptoanalyzer.data.WebDataFrame;
 import com.cryptoanalyzer.files.BufferedWriterCreator;
 
+import java.util.ArrayList;
+
 public class AnalysisExporter extends FileExporter {
+
+    private int count;
+    private float totalVolume;
+    private final ArrayList<Float> pricesOpen = new ArrayList<>();
+    private final ArrayList<Float> pricesClose = new ArrayList<>();
+
 
     public AnalysisExporter() {
         super();
@@ -28,15 +36,60 @@ public class AnalysisExporter extends FileExporter {
 
     @Override
     public void onBegin() {
+        count = 0;
+        totalVolume = 0;
+        pricesOpen.clear();
+        pricesClose.clear();
+
         super.onBegin();
     }
 
     @Override
     public void onSetData(WebDataFrame frame) {
+        count++;
+        totalVolume += frame.volume;
+        pricesOpen.add(frame.open);
+        pricesClose.add(frame.close);
     }
 
     @Override
     public void onEnd() {
+        writeToFile("====== VOLUME =======\n");
+        writeToFile("- Total Volume: " + totalVolume + "\n");
+        writeToFile("- Average Volume: " + totalVolume / count + "\n");
+
+        writeToFile("\n");
+
+        writeToFile("====== PRICE =======\n");
+
+        float changePercentage = (pricesClose.getLast() - pricesOpen.getFirst()) / pricesOpen.getFirst() * 100;
+
+        writeToFile("- Change: " + changePercentage + "%\n");
+
+        float mean = 0;
+
+        for (float price : pricesClose) {
+            mean += price;
+        }
+
+        mean /= count;
+
+        float variance = 0;
+
+        for (float price : pricesClose) {
+            float diff = price - mean;
+            variance += diff * diff;
+        }
+
+        variance /= count;
+
+        float std = (float) Math.sqrt(variance);
+        float t = (float) count / 24;
+        float volatility = std * (float) Math.sqrt(t);
+        float volatilityPercentage = volatility / pricesClose.getLast() * 100;
+
+        writeToFile("- Monthly Volatility: " + volatility + " = " + volatilityPercentage + "%\n");
+
         super.onEnd();
     }
 }
---
+++ src/test/java/com/cryptoanalyzer/exporters/AnalysisExporterTest.java
@@ -0,0 +1,36 @@
+package com.cryptoanalyzer.exporters;
+
+import com.cryptoanalyzer.data.DataManager;
+import com.cryptoanalyzer.data.WebDataFrame;
+import com.cryptoanalyzer.files.StringBufferedWriterCreator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnalysisExporterTest {
+
+    @Test
+    void testOneFrameSave() {
+        StringBufferedWriterCreator creator = new StringBufferedWriterCreator();
+        AnalysisExporter exporter = new AnalysisExporter(creator);
+
+        DataManager.getInstance().addObserver(exporter);
+        DataManager.getInstance().initRepo();
+        DataManager.getInstance().pushWebDataFrame(new WebDataFrame("BTC_USD", 1700000000, 10, 11, 20, 4, 3));
+        DataManager.getInstance().pushWebDataFrame(new WebDataFrame("BTC_USD", 1700000000, 10, 13, 20, 4, 3));
+        DataManager.getInstance().pushWebDataFrame(new WebDataFrame("BTC_USD", 1700000000, 10, 15, 20, 4, 3));
+        DataManager.getInstance().flushRepo();
+
+        final String expected = """
+                ====== VOLUME =======
+                - Total Volume: 9.0
+                - Average Volume: 3.0
+                
+                ====== PRICE =======
+                - Change: 50.0%
+                - Volatility: 0.57735026
+                """;
+
+        assertEquals(expected, creator.getString());
+    }
+}
